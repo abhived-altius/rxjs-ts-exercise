86416c58065f17884b5c93003d2b7562724508f8
Abhived <abhived.m@altiushub.com>
2025-08-12 11:53:06 +0530

    Create interface Car and generator function.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
-import {interval} from "rxjs";
+import { SrvRecord } from "dns";
 
-console.log('Hello RxJS + TypeScript');
-
-const sub = interval(1000).subscribe((n) => 
+interface Car
 {
-    console.log("tick",n);
-});
+    name: String;
+    model: String;
+    yearOfRelease:  number;
+    brand:String;
+    color:String;
+}
 
-setTimeout(() => 
+function getRandomCar(): Car
 {
-    sub.unsubscribe();
-    console.log('Done');
+    const names = ["Swift", "Civic", "Mustang", "Corolla", "Model S"];
+    const models = ["LX", "Sport", "Deluxe", "Pro", "GT"];
+    const brands = ["Suzuki", "Honda", "Ford", "Toyota", "Tesla"];
+    const colors = ["red", "blue", "black", "white", "green"];
 
-}, 3500);
+    return {
+        name : names[Math.floor(Math.random() * names.length)],
+        model : models[Math.floor(Math.random() * models.length)],
+        yearOfRelease : Math.floor(Math.random() * (1980-2025+1))+1980,
+        brand: brands[Math.floor(Math.random() * brands.length)],
+        color: colors[Math.floor(Math.random() * colors.length)]
+    };
+}
+
+console.log(getRandomCar());
